10850e2fd4ac8e4eae79bc5e41dd82148faf0936
Emichira <emmanuelmichira@gmail.com>
2018-03-25 20:38:52 +0300

    Add template files

--- app/views.py
+++ app/views.py
@@ -4,8 +4,4 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template("base.html")
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
+    return render_template("index.html")
